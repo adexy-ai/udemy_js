6059aecb2acc05ce27e55b699af394cf4218f512
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-17 11:58:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+let Name = "anGeLa" //prompt("What is your name? ");
+let firstLetter = (Name.slice(0,1));
+let otherNamePart = (Name.slice(1, Name.length)) 
+
+console.log("Hello " + firstLetter.toUpperCase() + otherNamePart.toLowerCase());
+
+let dogAge = 12 //prompt("What is your dag age? ");
+let humanAge = (dogAge - 2) * 4 + 21;
+console.log("The age of the human is " + humanAge);
+
+function getMilk(money) {   
+    let bottle = money / 1.5;
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+    console.log("enterHouse");
+    console.log(Math.floor(bottle))
+}
+
+getMilk(10);
